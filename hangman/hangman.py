9a07c4d1343d57e30efa0cdecba0cd3fe29ccd68
hen <whshle08@gmail.com>
2024-03-03 03:25:46 -0800

    basic program
    no lives/tries system

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -3,5 +3,26 @@ from random import randint
 from tutorial import tutorial
 
 chosenWord = words[randint(0,99)]
+chosenWord = chosenWord.lower()
 
-tutorial()
+# tutorial()
+
+def game(word):
+
+    hiddenWord = []
+    for i in range(0, len(word)):
+        hiddenWord.append('_')
+
+    while ('_' in hiddenWord):
+        print(''.join(hiddenWord) + '\n')
+        
+        letter = input('Guess a letter!\n\n')
+
+        for i in range(0, len(word)):
+            if letter == word[i]:
+                hiddenWord[i] = word[i]
+        
+    print('\nGGEZ!!')
+        
+game(chosenWord)
+
