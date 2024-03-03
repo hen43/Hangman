dc2b6248ea6b5d6b9b2d24322966e903a6452303
hen <whshle08@gmail.com>
2024-03-03 03:39:38 -0800

    life system
    life support

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -8,21 +8,33 @@ chosenWord = chosenWord.lower()
 # tutorial()
 
 def game(word):
+    
+    tries = 6
 
     hiddenWord = []
     for i in range(0, len(word)):
         hiddenWord.append('_')
 
-    while ('_' in hiddenWord):
+    while ('_' in hiddenWord and tries > 0):
+        print(f'{tries} tries remaining.')
         print(''.join(hiddenWord) + '\n')
         
+        found = False
+        
         letter = input('Guess a letter!\n\n')
-
+    
         for i in range(0, len(word)):
             if letter == word[i]:
                 hiddenWord[i] = word[i]
+                found = True
+            
+        if found == False:
+            tries -= 1
         
-    print('\nGGEZ!!')
+    if '_' in hiddenWord:
+        print('nice try bro')
+    else:
+        print('LETS GOOOOO!!!! GGEZZZZZZ!!!')
         
 game(chosenWord)
 
